041885b60859fe9d9acfe66fb71795cb03a577b6
igarashi-ryo <55534019+igarashi-ryo@users.noreply.github.com>
2021-06-26 12:19:56 +0900

    [add]拳追加

---
+++ Waruwaru/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    int m_kobushiSwingMaxNum = 10;
+
+    GameObject m_kobushi = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_kobushi = GameObject.Find("Kobushi");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Kobushi kobushi = m_kobushi.GetComponent<Kobushi>();
+
+        if (m_kobushiSwingMaxNum == kobushi.GetSwingNum() && kobushi.GetState() == Kobushi.KobushiState.end)
+        {
+            //リザルト
+            Debug.Log("リザルトへ遷移");
+        }
+    }
+}
---
+++ Waruwaru/Assets/Scripts/Kobushi.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kobushi : MonoBehaviour
+{
+    public enum KobushiState
+    {
+        idle,
+        swing,
+        end,
+    }
+
+    KobushiState m_state = KobushiState.idle;
+
+
+    Vector3 m_offsetPos;
+
+    [SerializeField]
+    float m_speed = 1.0f;
+
+   int m_swingNum = 0;
+
+    [SerializeField]
+    float m_swingEndPosY = -5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_offsetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Operation();
+        switch (m_state)
+        {
+            case KobushiState.idle:
+                Idle();
+                break;
+
+            case KobushiState.swing:
+                Swing();
+                break;
+
+            case KobushiState.end:
+                End();
+                break;
+        }
+
+    }
+
+    //キーボード操作
+    void Operation()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(m_state == KobushiState.idle)
+            {
+                m_state = KobushiState.swing;
+
+                m_swingNum++;
+            }
+        }
+    }
+
+    //振り下ろし
+    void Swing()
+    {
+        transform.position -= new Vector3(0, m_speed * Time.deltaTime, 0);
+
+        if(transform.position.y < m_swingEndPosY)
+        {
+            m_state = KobushiState.end;
+        }
+    }
+
+    void Idle()
+    {
+
+    }
+
+    void End()
+    {
+        transform.position += new Vector3(0, m_speed * Time.deltaTime, 0);
+
+        if(transform.position.y > m_offsetPos.y)
+        {
+            transform.position = m_offsetPos;
+            m_state = KobushiState.idle;
+        }
+    }
+
+
+//=====================================================================================
+    
+    public int GetSwingNum()
+    {
+        return m_swingNum;
+    }
+
+    public KobushiState GetState()
+    {
+        return m_state;
+    }
+
+}
