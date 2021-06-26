c5697435d0d4e3d5fc160415b5e2784d7f3f8b0b
igarashi-ryo <55534019+igarashi-ryo@users.noreply.github.com>
2021-06-26 12:44:53 +0900

    [add]GameManager:拳降った回数でリザルトへ遷移する [add]Waremono生成用オブジェクト

---
+++ Waruwaru/Assets/Scripts/WaremonoCreater.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaremonoCreater : MonoBehaviour
+{
+    [SerializeField]
+    List<GameObject> m_waremonoPrefabs = new List<GameObject>();
+
+    Vector3 m_createPos;
+
+    [SerializeField]
+    float m_interval = 0;
+    float m_time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_createPos = transform.Find("CreatePos").position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_time > m_interval)
+        {
+            Instantiate(m_waremonoPrefabs[Random.Range(0, 2)]).transform.position = new Vector3(m_createPos.x,m_createPos.y,m_createPos.z);
+
+            m_time = 0;
+        }
+
+        m_time += Time.deltaTime;
+    }
+}
